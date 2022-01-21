d0141dcd62918462a052cac3084e0f84788cd1be
Sukhamjot Singh <Sukhamjot.Singh@iiitb.org>
2022-01-21 14:57:01 +0530

    full project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ToppersComponent } from './toppers/toppers.component';
+import { UsersComponent } from './users/users.component';
+import { WinnersComponent } from './winners/winners.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path : 'app-users',component: UsersComponent
+  },
+  {
+    path : 'app-winners', component: WinnersComponent
+  },
+  {
+    path: 'app-toppers', component : ToppersComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import {MatDialogModule} from '@angular/material/dialog';
+import { UsersComponent } from './users/users.component';
+import { WinnersComponent } from './winners/winners.component';
+import { ToppersComponent } from './toppers/toppers.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    WinnersComponent,
+    ToppersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    HttpClientModule,
+    MatTableModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
